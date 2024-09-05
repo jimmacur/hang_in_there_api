f8b87672104a463f77e3deae06560abe2faa8622
Shawn Thompson <thompson_shawn@hotmail.com>
2024-09-05 13:05:06 -0500

    feat: added gem serializer

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -1,12 +1,14 @@
 class Api::V1::PostersController < ApplicationController
   def index
     posters = Poster.all
-    render json: PosterSerializer.format_posters(posters)
+    options = {}
+    options[:meta] = {count: posters.length}
+    render json: PosterSerializer.new(posters, options)
   end
 
   def show
     poster = Poster.find(params[:id])
-    render json: PosterSerializer.format_poster(poster)
+    render json: PosterSerializer.new(poster)
   end
 
   # def create
